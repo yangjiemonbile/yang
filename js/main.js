028e054c2a005717874d11c42e0676d0176caa45
jie <1226033831@qq.com>
2022-05-18 18:04:38 +0800

    更改动画速度

--- js/main.js
+++ js/main.js
@@ -131,55 +131,55 @@ navItems.forEach((navItem) => {
 
 ScrollReveal({
   distance: "60px",
-  duration: 2000,
+  duration: 1500,
   delay: 100,
 });
 
 ScrollReveal().reveal(".home .info h2,.section-title-01,.section-title-02", {
-  delay: 500,
+  delay: 300,
   origin: "left",
 });
 ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", {
-  delay: 600,
+  delay: 400,
   origin: "right",
 });
 ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
 ScrollReveal().reveal(".media-icons i,.contact-left li", {
-  delay: 500,
+  delay: 300,
   origin: "left",
-  interval: 200,
+  interval: 100,
 });
 ScrollReveal().reveal(".home-img, .about-img", { delay: 500, origin: "bottom" });
 ScrollReveal().reveal(".about .descrption, .contact-right", {
-  delay: 600,
+  delay: 300,
   origin: "right",
 });
 ScrollReveal().reveal(".about .professional-list li", {
-  delay: 500,
+  delay: 300,
   origin: "right",
-  interval: 200,
+  interval: 80,
 });
 ScrollReveal().reveal(
   ".skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2",
   {
-    delay: 700,
+    delay: 400,
     origin: "left",
   }
 );
 ScrollReveal().reveal(
   ".experience-card, .service-card, .education, .portfolio .img-card",
   {
-    delay: 800,
+    delay: 400,
     origin: "bottom",
-    interval: 200,
+    interval: 100,
   }
 );
 ScrollReveal().reveal(
     "footer .group",
     {
-      delay: 500,
+      delay: 300,
       origin: "top",
-      interval: 200,
+      interval: 80,
     }
   );
   
